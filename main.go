f8120fc3772e8399f59cd92906d1a2e71b2b05f3
Dante Alighierin <zhivchikovv2@gmail.com>
2020-06-03 13:54:03 +0300

    uploading files func

--- main.go
+++ main.go
@@ -5,5 +5,19 @@ import {
 	"fmt"
 }
 
+
+func uploadFile(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Uploading File")
+}
+
+
+func setupRotes(){
+	http.HandleFunc("/upload", uploadFile")
+	http.ListenAndServe("8000",nil)
+}
+
+
 func main(){
 	fmt.Println("Go File Upload")
+	setupRoutes()
+}

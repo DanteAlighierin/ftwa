3fad393b30708b639562e0c510cf53d176b3fe21
Dante Alighierin <zhivchikovv2@gmail.com>
2020-06-03 15:11:54 +0300

    uploaded file func

--- main.go
+++ main.go
@@ -6,7 +6,39 @@ import (
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Uploading File")
+	fmt.Fprintf(w, "Uploading File\n")
+
+	///1. parse input, , type multipart/form-data
+	r.ParseMultipartForm(10 << 20)
+
+	//2. retrieve file from posted form data
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error")
+		fmt.Println("err")
+		return
+	}
+	defer fiele.Close()
+	fmt.Printf("Uploaded file: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Filename)
+	fmt.Printf("MIME type: %+v\n", handler.Header)
+
+	//3. write temp file to server
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tempFile.Write(fileBytes)
+
+	//4. return whether or not  this has ben succesful
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
 
 func setupRoutes() {

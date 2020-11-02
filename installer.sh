07c15f509c82083e4cb86aab538fc8a7b6cf33db
Dante Alighierin <zhivchikovv2@gmail.com>
2020-11-02 22:35:14 +0000

    installer for void and slackware

--- installer.sh
+++ installer.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/bash
+
+
+cat /etc/*-release
+
+
+if grep -q void /etc/*-release
+
+then sudo xbps-install go libressl
+
+fi
+
+
+if grep -q slackware /etc/*-release
+
+then sudo slackpkg install gcc-go openssl
+
+fi

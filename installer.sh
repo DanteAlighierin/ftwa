96f8d7778728616f6ca68678853a054c8fedf919
Dante Alighierin <zhivchikovv2@gmail.com>
2020-11-02 22:56:21 +0000

    installer for debian based dystro

--- installer.sh
+++ installer.sh
@@ -5,20 +5,20 @@ cat /etc/*-release
 
 
 if grep -q void /etc/*-release
-
 then sudo xbps-install go libressl
-
 fi
 
 
 if grep -q slackware /etc/*-release
-
 then sudo slackpkg install gcc-go openssl
-
 fi
 
+
 if grep -q nixos /etc/*-release
-
 then nix-env -i gcc-go openssl
+fi
 
+
+if grep -q ubuntu /etc/*-release
+then sudo apt install golang openssl
 fi

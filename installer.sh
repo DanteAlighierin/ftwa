5243a5ddbdd92a3b82e65c3cc7a593a618697ec7
Dante Alighierin <zhivchikovv2@gmail.com>
2020-11-03 12:39:36 +0000

    run cert generator, conclusion about success

--- installer.sh
+++ installer.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/bash
 
 
-cat /etc/*-release
+cat /etc/*-release #find out the distro
 
 
-if grep -q void /etc/*-release
-then sudo xbps-install go libressl
+if grep -q void /etc/*-release #Search by keyword
+then sudo xbps-install go libressl #Installing packages using the package manager of a specific distro
 fi
 
 
-if grep -q slackware /etc/*-release
-then sudo slackpkg install gcc-go openssl
+if grep -q slackware /etc/*-release #Search by keyword
+then sudo slackpkg install gcc-go openssl #Installing packages using the package manager of a specific distro
 fi
 
 
-if grep -q nixos /etc/*-release
-then nix-env -i gcc-go openssl
+if grep -q nixos /etc/*-release #Search by keyword
+then nix-env -i gcc-go openssl #Installing packages using the package manager of a specific distro
 fi
 
 
-if grep -q ubuntu /etc/*-release
-then sudo apt install golang openssl
+if grep -q ubuntu /etc/*-release #Search by keyword
+then sudo apt install golang openssl #Installing packages using the package manager of a specific distro
 fi
+
+
+./cert.sh #generate certificates
+
+
+echo 'Dependencies satisfied, certificates generated' 
+echo "Enter 'go run main.go' to start the service"

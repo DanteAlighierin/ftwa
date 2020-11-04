a164976ba4a24f58028522cc34c75e5f84cbd230
Dante Alighierin <zhivchikovv2@gmail.com>
2020-11-04 19:02:03 +0000

    arch based dystro inst.; or operand

--- installer.sh
+++ installer.sh
@@ -19,9 +19,16 @@ then nix-env -i gcc-go openssl #Installing packages using the package manager of
 fi
 
 
-if grep -q ubuntu /etc/*-release #Search by keyword
+if grep -q ubuntu /etc/*-release || if grep -q deb /etc/*-release || if grep -q mint /etc/*-release #Search by keyword
 then sudo apt install golang openssl #Installing packages using the package manager of a specific distro
 fi
+
+
+
+if grep -q arch /etc/*-release || if grep -q manjaro /etc/*-release #Search by keyword
+then sudo pacman -S golang openssl #Installing packages using the package manager of a specific distro
+fi
+
 
 
 ./cert.sh #generate certificates

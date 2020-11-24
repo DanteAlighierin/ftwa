4f5512b7904c826dd408c2a072fb69533a100cc3
Dante Alighierin <zhivchikovv2@gmail.com>
2020-11-24 22:54:54 +0000

    link styles to downloads page

--- downloads.sh
+++ downloads.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 cd static/temp-images
-tree -H '' --noreport --charset utf-8 >> indexses.html
-echo 'hello'
+
+tree -H '' --noreport --charset utf-8 > indexses.html
+sed -i 's_<style type="text/css">_<style type="text/css" href="style.css">_' indexses.html
+
+

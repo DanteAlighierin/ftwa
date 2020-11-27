0a1001ccbc85bafd90166bbb11932a9546e56323
Dante Alighierin <zhivchikovv2@gmail.com>
2020-11-27 20:38:49 +0000

    fix back forwading

--- downloads.sh
+++ downloads.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-cd static/temp-images
+cd static/
 
-tree -H '' --noreport --charset utf-8 > indexses.html
+tree  temp-images -H 'temp-images'  --noreport --charset utf-8 > indexses.html
 
 sed -i 's_<style type="text/css">_<link rel="stylesheet" type="text/css" href="style.css">_' indexses.html
 
@@ -11,6 +11,7 @@ sed -i '8i<meta name ="viewport" content="width=device-width, initial-scale=1">'
 
 sed -i 's_<h1>Downloads</h1><p>_<header><nav class="container"><a class="logo" href="../"><span>F</span><span>T</span><span>W</span><span>A</span></a><div class="nav-toggle"><span></span></div><ul id="menu"><li><a href="">About</a></li><li><a href="/temp-images">Downloads</a></li></ul></nav></header>_' indexses.html
 
+sed -i '/<a href="temp-images">te/d' indexses.html
 sed -i '/Steve/d' indexses.html
 sed -i '/Moore/d' indexses.html
 sed -i '/Rocher/d' indexses.html

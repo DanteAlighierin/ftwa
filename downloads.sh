ce53576955a4b794de3f388a597ce133ca4c315b
Dante Alighierin <zhivchikovv2@gmail.com>
2020-12-08 22:32:41 +0000

    fix theme switcher

--- downloads.sh
+++ downloads.sh
@@ -11,14 +11,14 @@ sed -i '8i<meta name ="viewport" content="width=device-width, initial-scale=1">'
 
 sed -i 's_<p class="VERSION">_<script src="/u/jquery.min.js"></script><script src="/u/switcher.js"></script>_' downloads.html
 
-sed -i '32i<ol class="rounded">' downloads.html
+sed -i '33i<ol class="rounded">' downloads.html
 
 #sed -i "35i</ol>" downloads.html
 
 sed -i 's_</a><br></br>_</a><ol/><br></br>_' downloads.html
 
 
-sed -i 's_<h1>Downloads</h1><p>_<header><nav class="container"><a class="logo" href="../"><span>F</span><span>T</span><span>W</span><span>A</span></a><div class="nav-toggle"><span></span></div><ul id="menu"><div class="switch">Dark Mode:   <span class="inner-switch">OFF</span></div><li><a href="">About</a></li><li><a href="u/index.html">Upload</a></li></ul></nav></header><a href="home.html" class="home">Go Back</a>_' downloads.html
+sed -i 's_<h1>Downloads</h1><p>_<header><nav class="container"><a class="logo" href="../"><span>F</span><span>T</span><span>W</span><span>A</span></a><div class="nav-toggle"><span></span></div><ul id="menu"><div class="text">Dark Mode:   </div><div class="toggle-switch"><label class="switch"><input id="theme-switch" type="checkbox"><span class="slider round"></span></span></div><li><a href="">About</a></li><li><a href="u/index.html">Upload</a></li></ul></nav></header><a href="home.html" class="home">Go Back</a>_' downloads.html
 
 sed -i '/<a href="temp-images">te/d' downloads.html
 sed -i '/Steve/d' downloads.html
--- static/u/switcher.js
+++ static/u/switcher.js
@@ -1,15 +1,58 @@
-    $( ".inner-switch" ).on("click", function() {
+const themeSwitcher = document.getElementById("theme-switch");
 
-      if( $( "body" ).hasClass( "dark" )) {
 
-        $( "body" ).removeClass( "dark" );
+themeSwitcher.checked = false;
 
-        $( ".inner-switch" ).text( "OFF" );
+function clickHandler() {
 
-      } else {
+    if (this.checked) {
 
-        $( "body" ).addClass( "dark" );
+        document.body.classList.remove("light");
 
-        $( ".inner-switch" ).text( "ON" );     }
-   }
-      )
+        document.body.classList.add("dark");
+
+        localStorage.setItem("theme", "dark");
+
+    } else {
+
+        document.body.classList.add("light");
+
+        document.body.classList.remove("dark");
+
+        localStorage.setItem("theme", "light");
+
+    }
+
+}
+
+themeSwitcher.addEventListener("click", clickHandler);
+
+
+
+window.onload = checkTheme();
+
+
+
+function checkTheme() {
+
+    const localStorageTheme = localStorage.getItem("theme");
+
+
+
+    if (localStorageTheme !== null && localStorageTheme === "dark") {
+
+        // set the theme of body
+
+        document.body.className = localStorageTheme;
+
+
+
+        // adjust the slider position
+
+        const themeSwitch = document.getElementById("theme-switch");
+
+        themeSwitch.checked = true;
+
+    }
+
+}

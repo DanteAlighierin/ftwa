5e8641caaefe6dbf6c99bd12fa151ae1e90842ec
Dante Alighierin <zhivchikovv2@gmail.com>
2020-12-11 20:46:58 +0000

    fix https prefix in qr

--- qr.sh
+++ qr.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-curl -s https://myexternalip.com/raw > out.txt && echo :8080 >> out.txt && cat out.txt | qrencode -t ansiutf8
+echo https:// > out.txt
+curl -s https://myexternalip.com/raw >> out.txt && echo :8080 >> out.txt && cat out.txt | qrencode -t ansiutf8

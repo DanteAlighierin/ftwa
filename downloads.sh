08602bb48512da7c0f0672f00b268d92dfab8ed6
Dante Alighierin <zhivchikovv2@gmail.com>
2020-12-15 20:36:07 +0000

    theme switcher fully applied in mobile version

--- downloads.sh
+++ downloads.sh
@@ -7,11 +7,13 @@ sed -i 's_<style type="text/css">_<link rel="stylesheet" type="text/css" href="d
 
 sed -i 's_Directory Tree_Downloads_' downloads.html
 
+sed -i '/<a href="temp-images">temp-images</d' downloads.html
+
 sed -i '8i<meta name ="viewport" content="width=device-width, initial-scale=1">' downloads.html
 
-sed -i 's_<p class="VERSION">_<script src="/u/jquery.min.js"></script><script src="/u/switcher.js"></script>_' downloads.html
+sed -i 's_<p class="VERSION">_<div class="circle"><input id="cSwitch" type="checkbox"></div></div><script src="/u/jquery.min.js"></script><script src="/u/switcher.js"></script>_' downloads.html
 
-sed -i '33i<ol class="rounded">' downloads.html
+sed -i '32i<ol class="rounded">' downloads.html
 
 #sed -i "35i</ol>" downloads.html
 
@@ -20,7 +22,6 @@ sed -i 's_</a><br></br>_</a><ol/><br></br>_' downloads.html
 
 sed -i 's_<h1>Downloads</h1><p>_<header><nav class="container"><a class="logo" href="../"><span>F</span><span>T</span><span>W</span><span>A</span></a><div class="nav-toggle"><span></span></div><ul id="menu"><div class="text">Dark Mode:   </div><div class="toggle-switch"><label class="switch"><input id="theme-switch" type="checkbox"><span class="slider round"></span></span></div><li><a href="">About</a></li><li><a href="u/index.html">Upload</a></li></ul></nav></header><a href="home.html" class="home">Go Back</a>_' downloads.html
 
-sed -i '/<a href="temp-images">te/d' downloads.html
 sed -i '/Steve/d' downloads.html
 sed -i '/Moore/d' downloads.html
 sed -i '/Rocher/d' downloads.html

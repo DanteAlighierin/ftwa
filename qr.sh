38d1bd2838bd9aaa25bb2d44a3c2168a029cb23d
Dante Alighierin <zhivchikovv2@gmail.com>
2020-12-28 21:19:50 +0000

    fix qr output

--- qr.sh
+++ qr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 echo https:// > out.txt
-curl -s https://myexternalip.com/raw >> out.txt && echo :8080 >> out.txt && cat out.txt | qrencode -t ansiutf8
+curl -s https://myexternalip.com/raw >> out.txt && echo :8443 >> out.txt && cat out.txt | qrencode -t ansiutf8

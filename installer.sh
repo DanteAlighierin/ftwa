8ad542d2044086fefeb44e0166de33cdb4774147
Dante Alighierin <zhivchikovv2@gmail.com>
2021-02-11 16:09:32 +0300

    permissions for cert. installer

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 
 chmod +x qr.sh
-
+chmod +x cert.sh
 
 if grep void /etc/*-release #Search by keyword
 then sudo xbps-install go libressl qrencode#Installing packages using the package manager of a specific distro

454c544c2a7067c419b19b8aaccf86c015852501
Dante Alighierin <zhivchikovv2@gmail.com>
2021-03-10 21:25:56 +0300

    choice: auto or manual certificate generation

--- cert.sh
+++ cert.sh
@@ -4,4 +4,27 @@ openssl genrsa -out server.key 2048
 
 openssl ecparam -genkey -name secp384r1 -out server.key
 
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650 -subj "/C=EN/ST= /L= /O= /CN=localhost"
+while true; do
+	read -p "Do you wanna to autogenerate certs? " yn
+	case $yn in
+
+
+#select yn in "Yes" "No"
+#	case $yn in
+
+
+
+
+		[Yy]* ) openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650 -subj "/C=EN/ST= /L= /O= /CN=localhost"; echo "Certs generated!!!"; break;;
+		
+
+		[Nn]* ) openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650;;
+		* ) openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650 -subj "/C=EN/ST= /L= /O= /CN=localhost"; echo "Certs generated!!!"; break;;
+
+	esac
+done
+
+
+
+
+

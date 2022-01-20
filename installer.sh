56dbdab4f9e510dcf610e2a4064c86a917b83c81
Dante <zhivchikovv2@gmail.com>
2022-01-20 18:55:52 +0300

    fix

--- installer.sh
+++ installer.sh
@@ -1,4 +1,3 @@
-./pkg.sh
 
 #mkdir -p static/u static/temp-images
 
@@ -11,9 +10,10 @@ rm installer.sh
 chmod +x qr.sh
 chmod +x cert.sh
 
+./pkg.sh
 ./cert.sh #generate certificates
 
 
 echo "Dependencies satisfied, certificates generated"
-echo "Enter 'go run main.go' to start the service"
+echo "Go to 'ftwa' folder and enter 'go run main.go' to start the service"
 
